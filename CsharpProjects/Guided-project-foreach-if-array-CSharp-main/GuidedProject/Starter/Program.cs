26ac4d3d6a28072e201b278c5d5e4ddb0520ad13
DouglasAmos <145407035+DouglasAmos@users.noreply.github.com>
2024-01-13 22:15:11 -0500

    Update Program.cs

--- CsharpProjects/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ CsharpProjects/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -8,20 +8,46 @@ int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-int sophiaSum = 0;
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
-decimal sophiaScore;
-
-foreach (int score in sophiaScores){
-    sophiaSum += score;
-}
-
-//Test
-
-sophiaScore = (decimal)sophiaSum / currentAssignments;
+int[] studentScores = new int[10];
 
 Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+
+    int sumAssignmentScores = 0;
+
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        sumAssignmentScores += score;
+    }
+
+
+
+    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+
+    
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+
+}
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
