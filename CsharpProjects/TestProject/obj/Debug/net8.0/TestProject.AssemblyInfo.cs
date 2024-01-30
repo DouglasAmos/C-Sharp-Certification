6ad870f4a374872bb7def1521ad3ec6cc1e73b68
DouglasAmos <145407035+DouglasAmos@users.noreply.github.com>
2024-01-29 20:20:52 -0500

    branching and looping guided ex

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8f7680f5baf364759385cc04f230e3a5bed090b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8556b816649cd17a7145cc50a52a24f66ff8df2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
