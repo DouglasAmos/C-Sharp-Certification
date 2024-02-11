4cd6048275af188752ad22595d147e27cfa8bc69
DouglasAmos <douglas.amos1@gmail.com>
2024-02-11 17:30:28 -0500

    case 2

--- CsharpProjects/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/Program.cs
+++ CsharpProjects/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/Program.cs
@@ -1,4 +1,6 @@
 // the ourAnimals array will store the following: 
+using System.Globalization;
+
 string animalSpecies = "";
 string animalID = "";
 string animalAge = "";
@@ -19,64 +21,67 @@ string[,] ourAnimals = new string[maxPets, 6];
 // create some initial ourAnimals array entries
 
 //figure out why you needed to declare i here
-int i = 0;
-
-switch (i)
+for (int i = 0; i < maxPets; i++)
 {
-    case 0:
-        animalSpecies = "dog";
-        animalID = "d1";
-        animalAge = "2";
-        animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
-        animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
-        animalNickname = "lola";
-        break;
 
-    case 1:
-        animalSpecies = "dog";
-        animalID = "d2";
-        animalAge = "9";
-        animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
-        animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
-        animalNickname = "loki";
-        break;
+    switch (i)
+    {
+        case 0:
+            animalSpecies = "dog";
+            animalID = "d1";
+            animalAge = "2";
+            animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+            animalNickname = "lola";
+            break;
 
-    case 2:
-        animalSpecies = "cat";
-        animalID = "c3";
-        animalAge = "1";
-        animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
-        animalPersonalityDescription = "friendly";
-        animalNickname = "Puss";
-        break;
+        case 1:
+            animalSpecies = "dog";
+            animalID = "d2";
+            animalAge = "9";
+            animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+            animalNickname = "loki";
+            break;
 
-    case 3:
-        animalSpecies = "cat";
-        animalID = "c4";
-        animalAge = "?";
-        animalPhysicalDescription = "";
-        animalPersonalityDescription = "";
-        animalNickname = "";
-        break;
+        case 2:
+            animalSpecies = "cat";
+            animalID = "c3";
+            animalAge = "1";
+            animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
+            animalPersonalityDescription = "friendly";
+            animalNickname = "Puss";
+            break;
 
-    default:
-        animalSpecies = "";
-        animalID = "";
-        animalAge = "";
-        animalPhysicalDescription = "";
-        animalPersonalityDescription = "";
-        animalNickname = "";
-        break;
+        case 3:
+            animalSpecies = "cat";
+            animalID = "c4";
+            animalAge = "?";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+            break;
+
+        default:
+            animalSpecies = "";
+            animalID = "";
+            animalAge = "";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+            break;
+
+    }
+
+
+    ourAnimals[i, 0] = "ID #: " + animalID;
+    ourAnimals[i, 1] = "Species: " + animalSpecies;
+    ourAnimals[i, 2] = "Age: " + animalAge;
+    ourAnimals[i, 3] = "Nickname: " + animalNickname;
+    ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
+    ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
 
 }
-
-
-ourAnimals[i, 0] = "ID #: " + animalID;
-ourAnimals[i, 1] = "Species: " + animalSpecies;
-ourAnimals[i, 2] = "Age: " + animalAge;
-ourAnimals[i, 3] = "Nickname: " + animalNickname;
-ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
-ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
 
 
 
@@ -104,10 +109,117 @@ do
         menuSelection = readResult.ToLower();
     }
 
-    Console.WriteLine($"You selected menu option {menuSelection}.");
-    Console.WriteLine("Press the Enter key to continue");
+    // Console.WriteLine($"You selected menu option {menuSelection}.");
+    // Console.WriteLine("Press the Enter key to continue");
 
     // pause code execution
-    readResult = Console.ReadLine();
+    // readResult = Console.ReadLine();
+
+    switch (menuSelection)
+    {
+        case "1":
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < 6; j++)
+                    {
+                        Console.WriteLine(ourAnimals[i, j]);
+                    }
+                }
+            }
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+
+        case "2":
+            string anotherPet = "y";
+            int petCount = 0;
+
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    petCount += 1;
+                }
+            }
+
+            if (petCount < maxPets)
+            {
+                Console.WriteLine($"We currently have {petCount} pets that need homes. We can manage {(maxPets - petCount)} more.");
+            }
+
+            while (anotherPet == "y" && petCount < maxPets)
+            {
+                petCount += 1;
+
+
+                if (petCount < maxPets)
+                {
+                    Console.WriteLine("Do you want to enter info for another pet (y/n)");
+
+                    do
+                    {
+                        readResult = Console.ReadLine();
+                        if (readResult != null)
+                        {
+                            anotherPet = readResult.ToLower();
+                        }
+                    } while (anotherPet != "y" && anotherPet != "n");
+                }
+
+            }
+
+
+            if (petCount >= maxPets)
+            {
+
+                Console.WriteLine("We have reached our limit on the number of pets that we can manage.");
+                Console.WriteLine("Press the Enter key to continue.");
+                readResult = Console.ReadLine();
+            }
+
+            break;
+
+        case "3":
+            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+
+        case "4":
+            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+
+        case "5":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+
+        case "6":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+
+        case "7":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+
+        case "8":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+
+        default:
+            break;
+    }
 
 } while (menuSelection != "exit");
