a6b28b7b9936db9a86d836f2f6b004a00c030a21
DouglasAmos <douglas.amos1@gmail.com>
2024-02-25 12:26:35 -0500

    finished variable data guided project

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634d35ede216840ffe139ad9e8e3d8ca21d19dc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14d0c86dd9b2c14be502a8427f84166bcdfe09b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
